0707bfd17df709e0ebab0deabcb8f37ede784cf4
João Victor Pacheco <jvp1805@gmail.com>
2021-08-29 22:00:30 -0300

    Upando projeto para o git

---
+++ analisador_lexico/alguma-lexico/src/main/antlr4/br/ufscar/dc/compiladores/alguma/lexico/AlgumaLexer.g4
@@ -0,0 +1,63 @@
+lexer grammar AlgumaLexer;
+
+
+PALAVRA_CHAVE: 'algoritmo'| 'declare' | 'literal' | 'inteiro'| 'leia' |'escreva'|
+                'real'|'logico'| 'se' |'entao' | 'senao'|'fim_se'| 'e' | 'ou'|
+                 '..'| 'caso' | 'seja' | 'fim_caso'| 'para' | 'ate' | 'faca' |
+                 'fim_para'| 'enquanto' | 'fim_enquanto'| '&'| '^'| 'registro'|
+                 'fim_registro'| 'tipo'| 'procedimento' | 'fim_procedimento'|
+                 'var' | 'funcao' | 'fim_funcao'| 'retorne'| 'constante' |
+                 'falso' | 'nao' | 'verdadeiro'|
+                 'fim_algoritmo' ;
+
+NUM_INT: ('0'..'9')+;
+
+NUM_REAL: ('0'..'9')+ '.' ('0'..'9')+;
+
+IDENT: ('a'..'z' | 'A'..'Z')('a'..'z' | 'A'..'Z' |'0'..'9'| '_')*;
+
+ATRIB: '<-';
+
+OP_REL: '>' | '>=' | '<' | '<=' | '<>' | '=';
+
+OP_ARIT: '+' | '-' | '*' | '/' | '%';
+
+DELIM: ':';
+
+ABRECHA: '[';
+
+FECHACHA: ']';
+
+
+
+ABREPAR: '(';
+
+FECHAPAR: ')';
+
+VIRG: ',';
+
+PONT: '.';
+
+CADEIA: '"' (ESC_SEQ | ~('"' | '\\' |'\n') )* '"';
+
+CADEIA_NFECHADA: '"' (ESC_SEQ | ~('"' | '\\') )* '\n';
+
+fragment
+ESC_SEQ: '\\"';
+
+
+
+COMENTARIO: '{'  ~('\n')*? '}' -> skip;
+
+COMENT_ERRADO: '{'  ~('}')* '\n';
+
+WS: (' ' | '\t' | '\r' ) -> skip;
+
+PULAL: ('\n') -> skip;
+
+
+
+
+
+ERRO: .;
+
---
+++ analisador_lexico/alguma-lexico/src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
@@ -0,0 +1,72 @@
+/*
+ * Analisador Léxico 
+ * Autor: João Victor Pacheco RA:594970
+ *
+ * Descrição: Projeto de um Analisador Léxico para a disciplian de Construção 
+ * de Compiladores I, ministrada pelo professor Daniel Lucrédio no Semetre 
+ * ENPE 2020/2 - Projeto feito baseado nos exemplos de aula do professor, por
+ * isso alguns nomes de classes são iguais aos do professor, pois eu fui
+ * seguindo o vídeo e fazendo, junto com as minhas modificações nos códigos.
+ *
+ *Para compilar: 
+ * - Clique em File > Open Project 
+ * - Selecione o diretório onde você salvou o projeto, clique no nome do
+ * - projeto "alguma-lexico" e clique em Open Project
+ * - Selecione o projeto 'alguma-lexico' na aba Projects
+ *   (caso você tenha outros projetos nessa aba), clique com o botão direito
+ *   no nome do projeto e selecione "Build"  (ou "Clean and Build" caso seja a
+ *   primeira vez que for executar).
+ *
+ */
+package br.ufscar.dc.compiladores.alguma.lexico;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.LineNumberReader;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+
+/*  Classe Principal responsável pela execução do Analisador Lexico */
+public class Principal {
+    public static void main (String args[]) throws IOException{
+        CharStream cs = CharStreams.fromFileName(args[0]); /*Recebe o primeiro argumento passado (arquivo de input)*/
+        AlgumaLexer lex = new AlgumaLexer(cs); /* Abre o arquivo em lex*/
+        String path = args[1]; /*Recebe o segundo argumento (arquivo de saída)*/
+        File file = new File(path);/* Abre o arquivo de saída em file*/
+        Token t= null; /* Define uma variável token como Null*/
+        
+        
+        PrintStream stream = new PrintStream(file); 
+        /* Seta a escrita no arquivo de saída ao invés do terminal*/
+        System.setOut(stream);
+        
+        /* Enquanto não for Final do arquivo, lê os proximos caracteres e executa o analisador seguindo as regras definidas no arquivo AlgumaLexer.g4*/
+        while ((t=lex.nextToken()).getType() != Token.EOF){
+            String buff = AlgumaLexer.VOCABULARY.getDisplayName(t.getType());
+            if ( buff == "PALAVRA_CHAVE" | buff == "OP_ARIT" | buff == "OP_REL"){ /* Caso seja uma palavra chave, operador aritmético ou operador relacional, deve imprimir o que foi lido e o tipo deverá ter o mesmo nome do que foi lido*/
+                System.out.println("<'"+t.getText()+"','"+t.getText()+"'>");
+            }else{
+            if (buff == "ERRO"){ /* Caso detecte um erro de simbolo não identificado, mostra a mensagem com a linha de ocorrencia e para execução*/
+                System.out.println("Linha "+t.getLine()+": "+t.getText()+" - simbolo nao identificado");
+                break;}
+            else{
+            if (buff == "COMENT_ERRADO"){ /* Caso detecte um comentario não fechado, mostra a mensagem com a linha de ocorrencia e para a execução */
+                System.out.println("Linha "+t.getLine()+": comentario nao fechado");
+                break;
+            }
+                else{
+                    if (buff== "CADEIA_NFECHADA"){ /* Casp detecte uma cadeia literal não fechada na mesma linha, mostra a mensagem com a linha de ocorrência e para a execução*/
+                        System.out.println("Linha "+t.getLine()+": cadeia literal nao fechada");
+                        break;}
+                    else{ /*Trata as outras regras léxicas*/
+                        System.out.println("<'"+t.getText()+"',"+AlgumaLexer.VOCABULARY.getDisplayName(t.getType())+">");
+                    }}}}
+        }
+    
+    }
+}
+
+
